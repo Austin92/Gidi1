55851ef885f66a482464d2bbfdae553df229cc0a
Austine <aanyangbeso@yahoo.co.uk>
2017-12-07 00:33:31 +0000

    Gidi_Test

---
+++ app/src/main/java/com/example/aanya/gidi/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.aanya.gidi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn,btnRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SignIn.class);
+                startActivity(intent);
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Register.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
